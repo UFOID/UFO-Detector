312c21803a9ef8e7dcb8d7a004c5d8caa17fcf8e
UFOID <projectce@tutanota.de>
2017-03-21 09:15:33 +0000

    Added test resource folder to mockConfig

--- ufo-detector-engine/test/mock/mockconfig.cpp
+++ ufo-detector-engine/test/mock/mockconfig.cpp
@@ -22,6 +22,8 @@
 QString mockConfigResultDataDir;
 int mockConfigResultVideoCodec;
 QString mockConfigResultVideoCodecStr;
+QString testResourceFolder();
+
 
 Config::Config(QObject *parent) {
     Q_UNUSED(parent);
@@ -86,12 +88,17 @@ int Config::minPositiveDetections() {
 }
 
 QString Config::birdClassifierTrainingFile() {
+
+    return testResourceFolder()+"\\cascade.xml";
+}
+
+QString testResourceFolder(){
     QDir resources(QApplication::applicationDirPath());
     resources.cdUp();
     resources.cdUp();
     resources.cdUp();
     resources.cd("test\\resources");
-    return resources.path()+"\\cascade.xml";
+    return resources.path();
 }
 
 QString Config::resultDataFile() {
